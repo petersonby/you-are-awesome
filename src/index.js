3f99a464effb3f47c02b8316e2d18eca1482cbd8
Peterson <contact@peterson.by>
2018-10-03 17:27:54 +0300

    ten functions implementation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (prop) => prop;
+const createNotEnumerableProperty = (prop) => Symbol(prop);
+const createProtoMagicObject = () => Function;
+
+let increment = 0;
+const incrementor = () => {
+  increment++;
+  incrementor.valueOf = () => increment;
+
+  return incrementor;
+};
+
+let asyncIncrement = 0;
+const asyncIncrementor = () => {
+  return new Promise(resolve => {
+    asyncIncrement++;
+    resolve(asyncIncrement);
+  });
+};
+
+const createIncrementer = function* () {
+  let increment = 1;
+
+  while(true) {
+    yield increment++;
+  }
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = (prop) => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(prop), 1000);
+  });
+};
+
+const getDeepPropertiesCount = (obj) => {
+  let count = JSON.stringify(obj).match(/[0-9]+/g).length;
+  return count;
+};
+
+const createSerializedObject = () => null;
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => arr.sort((a, b) => a.__proto__ - b.__proto__);
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
